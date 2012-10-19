586bcbde5632b2fd1648748f35508b7086b1ea4e
Jayson Ng <jaysonzeon@gmail.com>
2012-10-19 14:57:10 -0400

    Modified tab Icons

--- src/edu/madcourse/dancalacci/boggle/Multiplayer.java
+++ src/edu/madcourse/dancalacci/boggle/Multiplayer.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 public class Multiplayer extends TabActivity {
 
@@ -14,24 +15,27 @@ public class Multiplayer extends TabActivity {
         setContentView(R.layout.multiplayer_main);
  
         TabHost tabHost = getTabHost();
+        int iH = getTabWidget().getLayoutParams().height;
  
         // Tab for Received
         TabSpec receivedSpec = tabHost.newTabSpec("Received");
-        receivedSpec.setIndicator(this.getResources().getText(R.string.received_Label));
-        //receivedSpec.setIndicator("Received", getResources().getDrawable(R.drawable.multiplayer_tabs));
+        //receivedSpec.setIndicator(this.getResources().getText(R.string.received_Label));
+        receivedSpec.setIndicator("Received", getResources().getDrawable(R.drawable.multiplayer_tabs));
         Intent receivedIntent = new Intent(this, Multiplayer_Received.class);
         receivedSpec.setContent(receivedIntent);
  
         // Tab for Sent
         TabSpec sentSpec = tabHost.newTabSpec("Sent");
-        sentSpec.setIndicator(this.getResources().getText(R.string.sent_Label));
-        //sentSpec.setIndicator("Sent", getResources().getDrawable(R.drawable.multiplayer_tabs));
+        TextView sentTextView = new TextView(this); 
+        //sentSpec.setIndicator(this.getResources().getText(R.string.sent_Label));
+        sentSpec.setIndicator("Sent", getResources().getDrawable(R.drawable.multiplayer_tabs));
         Intent sentIntent = new Intent(this, Multiplayer_Sent.class);
         sentSpec.setContent(sentIntent);
  
         // Adding all TabSpec to TabHost
         tabHost.addTab(receivedSpec); // Adding sent tab
         tabHost.addTab(sentSpec); // Adding songs tab
+
     }
 	
 }
