41d5259eff6ca4a69d1926d39958e83d001b47a8
Jayson Ng <jaysonzeon@gmail.com>
2012-11-23 20:41:01 -0500

    added buttons to create chart

--- src/edu/madcourse/dancalacci/circletouch/Profile.java
+++ src/edu/madcourse/dancalacci/circletouch/Profile.java
@@ -1,7 +1,12 @@
 package edu.madcourse.dancalacci.circletouch;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Profile extends Activity{
 
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 }
